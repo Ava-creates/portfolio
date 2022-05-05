44fa65c5a24bf398248cb27659299587eb85355e
Ava-creates <avanitiwari2002@gmail.com>
2022-05-05 20:49:03 +0400

    jjkklk

--- tree.js
+++
@@ -1,9 +0,0 @@
-let flowers1 = document.getElementsByClassName('flowers_1');
-
-for(let i=0; i<flowers1.length;i++){
-    flowers1[i].addEventListener('click', flowersFunction1)
-}
-
-function flowersFunction1(){
-    console.log("fds");
-}
